1692f420d3cb144aefcbc45087838b5b2c4f65ee
AlexHuynh23 <alex.huynh.23@bishops.com>
2021-10-25 10:25:26 -0700

    Added Question Class. The class has basic information/functions. Get functions, init, and str.

---
+++ question.py
@@ -0,0 +1,27 @@
+class Question():
+    def __init__(self, prompt, posAnswers):
+        self.prompt = prompt
+        self.posAnswers = posAnswers
+        self.answerCount = {}
+        for answers in posAnswers:
+            self.answerCount[answers] = 0
+
+    def __str__(self):
+        s = "Prompt: %s"
+        i = 1
+        for answers in self.posAnswers:
+            s += "\nAnswer %d: %s" % (i, answers)
+        return s 
+    
+    def getAnswers(self):
+        pass
+
+    def getPosAnswers(self):
+        return self.posAnswers
+
+    def getPrompt(self):
+        return self.prompt
+    
+    
+
+
